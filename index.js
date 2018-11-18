048207a5d94383cbb9549f576ac3271f4d5315cc
Ben Burrell <benjaminjburrell@gmail.com>
2018-11-18 02:10:34 -0700

    Working on Prove09

--- index.js
+++ index.js
@@ -9,4 +9,43 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('public/index.html'))
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
-app.get('/results.ejs?', (req, res) => res.render('pages/results.ejs'))
+app.get('/results.ejs?', function (req, res){
+    echo(req);
+    res.render('pages/results.ejs')
+})
+
+function calculateRate(mailType, weight){
+    var price = 0;
+    switch(mailType){
+        case "stamped":{
+            price += .50;
+            if (weight > (1/16)){
+                price += (weight -1 ) * .21;
+            }
+            break;
+        }
+        case "metered":{
+            price += .47;
+            if (weight > (1/16)){
+                price += (weight - 1) * .21;
+            }
+            break;
+        }
+        case "flats":{
+            price += 1.00;
+            if (weight > (1/16)){
+                price += (weight - 1) * .21;
+            }
+            break;
+        }
+        case "package":{
+            price +=3.50;
+            if (weight > 4)
+                price += .25;
+            if (weight >8)
+                price += (weight - 8) * .35;
+            break;
+        }
+    }
+    return price;
+}
