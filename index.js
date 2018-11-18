16b0329494a990ce262c8ce7b68c7f8efa210489
Ben Burrell <benjaminjburrell@gmail.com>
2018-11-18 00:43:43 -0700

    Working on Prove09

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
+constapp = express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+app.url('/' function (req, res) {
+    res.send('index.html');
+})
